e05a5de84989ab7180328752b973c3bd2697ebe9
tahaphuong <phuonghata1995@gmail.com>
2020-08-04 08:32:47 +0700

    fixing api

---
+++ server/modules/users/index.js
@@ -0,0 +1,53 @@
+let model = require('../auth/model')
+const { verifyToken } = require("../utils");
+const template = require('../template')
+
+const handlers = {
+  async getUsers(req, res, next) {
+    try {
+      let { 
+        pageIndex = 1, 
+        pageSize = 10,
+      } = req.query
+
+      let accessToken = req.headers.authorization;
+
+      if (accessToken) {
+        let userData = verifyToken(accessToken);
+        req.user = await model.findById(userData._id);
+      } else {
+        throw new Error('Nguời dùng chưa xác thực')
+      }
+      // count = !!count
+      let skip = eval((pageIndex - 1) * pageSize)
+      let limit = eval(pageSize)
+
+      let interest = req.user.info.interest
+      let gender = req.user.info.gender
+
+      if(!req.user.like) 
+        req.user.like = []
+      if(!req.user.unlike) 
+        req.user.unlike = []
+      
+      let like = req.user.like
+      let unlike = req.user.unlike
+      let metUsers = [...like, ...unlike]
+      console.log(metUsers)
+      let conditions = {
+        'info.gender': interest, 
+        'info.interest': gender, 
+        '_id': {$nin: metUsers}
+      }
+      let items = await model
+        .find(conditions, {info: 1})
+        .skip(skip)
+        .limit(limit)
+
+      console.log(req.user)
+      res.json(template.successRes(items))
+    } catch(e) {next(e)}
+  }
+}
+
+module.exports = handlers
--- server/router.js
+++ server/router.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = new express.Router();
-const path = "path";
 
 const authHandlers = require("./modules/auth");
+const userHandlers = require("./modules/users");
 
 router.post("/api/auth/sign-up", authHandlers.signUp);
 
 router.post("/api/auth/sign-in", authHandlers.signIn);
+
+// User handler 
+router.get("/api/users", userHandlers.getUsers, authHandlers.authenticatedMiddleware);
 
 module.exports = router;
