6398c5e5ba7e3b115b1957a074513594663a0f00
tahaphuong <phuonghata1995@gmail.com>
2020-08-16 14:32:50 +0700

    done adding image

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require("./connect-mongo");
 const express = require("express");
 const bodyParser = require("body-parser");
 const session = require("express-session");
+const path = require("path");
 const cors = require('cors')
 
 const router = require("./router");
@@ -12,13 +13,15 @@ const {
   authenticatedMiddleware,
 } = require("./modules/auth");
 
-const { allowCrossOrigin } = require('./modules/utils')
-
 const app = express();
 
 const port = 9000;
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({
+  extended: true,
+  limit: '50mb'
+}));
+
 app.use(cors())
 app.use(
   session({
@@ -37,6 +40,13 @@ app.use((err, req, res, next) => {
   if (err)
     res.json(template.failedRes(err.message));
 });
+
+app.use(express.static("public"));
+
+// when deploy, use this
+// app.get('/', function(req, res) {
+//   res.render(path.join(__dirname, "public", "index.html"));
+// });
 
 app.listen(port, (err) => {
   console.log(err || `Server opened at port '${port}'`);
--- server/modules/utils.js
+++ server/modules/utils.js
@@ -18,24 +18,4 @@ function verifyToken(token) {
   return jwt.verify(token, JWT_SECRET);
 }
 
-// Add headers
-function allowCrossOrigin(req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,token');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-};
-
-module.exports = { hashMd5, signToken, verifyToken, allowCrossOrigin };
+module.exports = { hashMd5, signToken, verifyToken };
