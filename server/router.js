06b77e8ca0a3c5219a51a647af80939395fd7b5e
tahaphuong <phuonghata1995@gmail.com>
2020-08-18 09:43:04 +0700

    fix authenticate

--- server/router.js
+++ server/router.js
@@ -9,20 +9,20 @@ const uploadImage = require("./modules/external/uploadImage");
 const dev = require('./modules/dev')
 
 router.get("/api/auth/check-user", authHandlers.checkUser); 
-
 router.post("/api/auth/sign-up", authHandlers.signUp);
-
 router.post("/api/auth/sign-in", authHandlers.signIn);
 
 // User handler 
-router.get("/api/cards", userHandlers.getCards, authHandlers.authenticatedMiddleware);
-router.post("/api/like-unlike", userHandlers.likeAndUnlike, authHandlers.authenticatedMiddleware);
-router.get("/api/match", userHandlers.getMatches, authHandlers.authenticatedMiddleware);
+let authen = authHandlers.authenticatedMiddleware
+
+router.get("/api/cards", authen, userHandlers.getCards);
+router.post("/api/like-unlike", authen, userHandlers.likeAndUnlike);
+router.get("/api/match", authen, userHandlers.getMatches);
 
 // setting handler
-router.post("/api/reset-password", settingHandlers.resetPassword, authHandlers.authenticatedMiddleware);
+router.post("/api/reset-password", authen, settingHandlers.resetPassword);
 
 // other handler
-router.post("/api/upload-image", uploadImage, authHandlers.authenticatedMiddleware);
+router.post("/api/upload-image", authen, uploadImage);
 // router.get("/api/dev", dev);
 module.exports = router;
